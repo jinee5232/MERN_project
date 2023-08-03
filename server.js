4326587bc7de32ec692b5f534e25bcfce67a7918
jinee5232 <jinee5232@gmail.com>
2023-08-03 17:20:59 +0800

    Make changes for deployment

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const courseRoute =require("./routes").course;
 const passport = require("passport");
 require("./config/passport")(passport);
 const cors = require("cors");
+const path = require("path");//HUUU
+const port = process.env.PORT || 8080; //HUU PORT //process.env.PORT是Heroku自動動態設定，8080則是備案，如果前面沒運行則在8080
+
 
 //連結mongooDB
 mongoose.connect("mongodb://127.0.0.1/mernDB").then(() =>{
@@ -20,16 +23,20 @@ mongoose.connect("mongodb://127.0.0.1/mernDB").then(() =>{
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
+app.use(express.static(pate.join(__dirname,"client", "build")));//HUU
 
 app.use("/api/user",authRoute);
 //course route應該被保護
 //如果request header 內部沒有jwt，則request就會被視為是unauthorized
 app.use("/api/courses",passport.authenticate("jwt",{session:false}), courseRoute);
 
-//只有登入的人才能新增課程或註冊課程
-//驗證jwt是否有效
+if(process.env.NODE_ENV =="production" || process.env.NODE_ENV === "staging"){
+    app.get("*",(req,res) =>{
+        res.sendFile(path.join(__dirname,"client", "build", "index.html"));
+    });
+}//HUU
 
-app.listen(8080, () =>{
+app.listen(port, () =>{
     console.log("後端伺服器listen to port 8080");
 });
 
